4e072f20d6b3857ec8c51b18304ea85041ae3c5e
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-09-18 21:59:13 +0200

    add back dist folder

---
+++ server/dist/assets/BackButton-C1N_2dAm.js
@@ -0,0 +1,6 @@
+import{g as r,j as e,L as c}from"./index-DL9tVKVf.js";/**
+ * @license lucide-react v0.484.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],i=r("chevron-left",o),m=({to:s="",label:a="",className:t="",fullWidth:n=!1})=>e.jsxs(c,{to:s,className:`inline-flex items-center gap-3 bg-primary px-5 py-2 text-on-dark shadow-md hover:bg-primary-dark ${n?"w-full justify-center":""} ${t}`,children:[e.jsx(i,{className:"h-5 w-5"}),e.jsx("span",{className:"text-base",children:a})]});export{m as B};
